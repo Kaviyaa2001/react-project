fac5f0ef107536a0628df6a76c826ff653594370
Kaviyaa2001 <114096611+Kaviyaa2001@users.noreply.github.com>
2023-06-11 10:13:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from "react";
-import PreviewArea from "./components/PreviewArea";
+import React from 'react';
+import './App.css';
+import WorkArea from "./components/WorkArea";
+import OutputView from './components/OutputView';
+import PreviewArea from './components/PreviewArea';
 
-
-export default function App() {
+function App() {
   return (
-    <div className="bg-blue-100 pt-6 font-sans">
-      <div className="h-screen overflow-hidden flex flex-row  ">
-        <div className="flex-1 h-screen overflow-hidden flex flex-row bg-white border-t border-r border-gray-200 rounded-tr-xl mr-2">
-       
-        </div>
-        <div className="w-1/3 h-screen overflow-hidden flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2">
-          <PreviewArea />
-        </div>
-      </div>
+    <div className="container">
+      <div className="section"><WorkArea/></div>
+      <div className="section_1"> <OutputView/></div>
+      <div className="section_2"> <PreviewArea/></div>
     </div>
   );
 }
+
+export default App;

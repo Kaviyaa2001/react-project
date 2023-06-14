d86e5b959dead247611f40da3fcdb6c380001a5a
Kaviyaa2001 <114096611+Kaviyaa2001@users.noreply.github.com>
2023-06-14 14:52:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,21 @@ import React from "react";
 // import PreviewArea from "./components/PreviewArea";
 // import WorkArea from "./components/WorkArea";
 // import OutputArea from "./components/OutputArea";
-import PreviewArea from "./components/PreviewArea";
-import InputArea from "./components/InputArea";
-import WorkArea from "./components/WorkArea";
+
 import DragAndDrop from "./components/DragAndDrop";
+import "./App.css";
 
 export default function App() {
   return (
-    <div className="bg-blue-100 pt-6 font-sans">
+    /*<div className="bg-blue-100 pt-6 font-sans">
       <div className="h-screen overflow-hidden flex flex-row  ">
         <div className="flex-1 h-screen overflow-hidden flex flex-row bg-white border-t border-r border-gray-200 rounded-tr-xl mr-2">
        
         </div>
-        {/* <div className="w-1/3 h-screen overflow-hidden flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2">
-          <InputArea />
-        </div> */}
-        {/* <div className="w-1/3 h-screen overflow-hidden flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2">
-          <Workspace />
-        </div> */}
+        {}
+        {}
         <div className="w-1/3 h-screen overflow-hidden flex flex-row bg-white border-t border-l border-gray-200 rounded-tl-xl ml-2">
-          <PreviewArea />
+          
         </div>
         <br />
         <br />
@@ -30,6 +25,19 @@ export default function App() {
           <DragAndDrop />
         </div>
       </div>
+    </div>*/
+
+    <div class="container">
+  <div class="normal-div" id="div-1">
+    <DragAndDrop/>
+    
     </div>
+  <div class="normal-div" id="div-2">
+    
+    </div>
+  <div class="normal-div" id="div-3">
+   
+    </div>
+  </div>
   );
 }
